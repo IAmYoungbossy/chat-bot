efaebfff429f242e72a53c21c8292929cad1e038
Letam Bossman Barinua <letambarinua@gmail.com>
2025-02-02 14:38:21 +0100

    Create initial chatbot response

--- backend/src/services/conversationService.ts
+++ backend/src/services/conversationService.ts
@@ -3,9 +3,20 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const createConversationService = async () => {
-  return await prisma.conversation.create({
+  const conversation = await prisma.conversation.create({
     data: {},
   });
+
+  // Create Bot initial response
+  await prisma.message.create({
+    data: {
+      isUserMessage: false,
+      content: "How can I help you?",
+      conversationId: parseInt(conversation.id.toString()),
+    },
+  });
+
+  return conversation;
 };
 
 export const getConversationsService = async () => {
