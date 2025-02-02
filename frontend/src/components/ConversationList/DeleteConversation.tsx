759ff3f5170bf1f90bce5faf6d7e02ac5ed0ab38
Letam Bossman Barinua <letambarinua@gmail.com>
2025-02-02 17:40:17 +0100

    Add delete modal for deleting conversation

--- frontend/src/components/ConversationList/DeleteConversation.tsx
+++ frontend/src/components/ConversationList/DeleteConversation.tsx
@@ -11,39 +11,58 @@ import useConversationContext from "@/customHooks/useConversationContext";
 
 // Types
 import { DeleteConversationProps } from "./types/deleteConversation.type";
+import DeleteModal from "./DeleteModal";
+import { useState, MouseEvent, useCallback } from "react";
 
 const DeleteConversation = ({
   conversationId,
   activeConversationId,
 }: DeleteConversationProps) => {
+  const [open, setOpen] = useState(false);
+
   const { router, setError, setConversations } =
     useConversationContext();
 
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const handleDelete = useCallback(
+    (e: MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault();
+      e.stopPropagation();
+      deleteConversation({
+        router,
+        setError,
+        conversationId,
+        setConversations,
+        activeConversationId,
+      });
+    },
+    [conversationId, activeConversationId]
+  );
+  
   return (
-    <button
-      type="button"
-      title="Delete"
-      aria-label="Delete conversation"
-      onClick={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        deleteConversation({
-          router,
-          setError,
-          conversationId,
-          setConversations,
-          activeConversationId,
-        });
-      }}
-    >
-      <SvgIcon>
-        <path
-          className="hover:fill-red-600"
-          fill="#49454F"
-          d="M7 21q-.824 0-1.412-.587A1.93 1.93 0 0 1 5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .824-.587 1.413A1.93 1.93 0 0 1 17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2z"
-        ></path>
-      </SvgIcon>
-    </button>
+    <>
+      <button
+        type="button"
+        title="Delete"
+        aria-label="Delete conversation"
+        onClick={handleOpen}
+      >
+        <SvgIcon>
+          <path
+            className="hover:fill-red-600"
+            fill="#49454F"
+            d="M7 21q-.824 0-1.412-.587A1.93 1.93 0 0 1 5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .824-.587 1.413A1.93 1.93 0 0 1 17 21zM17 6H7v13h10zM9 17h2V8H9zm4 0h2V8h-2z"
+          ></path>
+        </SvgIcon>
+      </button>
+      <DeleteModal
+        open={open}
+        onDelete={handleDelete}
+        handleClose={handleClose}
+      />
+    </>
   );
 };
 
---
+++ frontend/src/components/ConversationList/DeleteModal.tsx
@@ -0,0 +1,118 @@
+import {
+  Box,
+  Modal,
+  Button,
+  Backdrop,
+  Typography,
+} from "@mui/material";
+import { MouseEvent } from "react";
+
+const buttonStyle = {
+  flexGrow: 1,
+  width: "100%",
+  maxWidth: 160,
+  fontSize: "14px",
+  borderWidth: "0",
+  fontWeight: "500",
+  paddingTop: "10px",
+  lineHeight: "20px",
+  borderRadius: "100px",
+  paddingBottom: "10px",
+  letterSpacing: "0.1px",
+};
+
+const style = {
+  p: 4,
+  width: 400,
+  top: "50%",
+  left: "50%",
+  boxShadow: 24,
+  borderRadius: 2,
+  paddingTop: "24px",
+  paddingBottom: "24px",
+  position: "absolute",
+  bgcolor: "background.paper",
+  transform: "translate(-50%, -50%)",
+};
+
+const DeleteModal = ({
+  open,
+  onDelete,
+  handleClose,
+}: {
+  open: boolean;
+  handleClose: () => void;
+  onDelete: (e: MouseEvent<HTMLButtonElement>) => void;
+}) => {
+  return (
+    <Modal
+      open={open}
+      onClose={handleClose}
+      slots={{ backdrop: Backdrop }}
+      aria-describedby="delete-conversation"
+      aria-labelledby="delete-conversation-modal"
+      slotProps={{
+        backdrop: {
+          sx: {
+            backdropFilter: "blur(5px)",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+          },
+        },
+      }}
+    >
+      <Box sx={style}>
+        <Typography
+          variant="h6"
+          component="h2"
+          sx={{
+            fontSize: "22px",
+            fontWeight: "400",
+            lineHeight: "28px",
+            textAlign: "center",
+            marginBottom: "24px",
+          }}
+          id="delete-conversation"
+        >
+          Are you sure you want to delete Conversation 1?
+        </Typography>
+        <Box
+          sx={{
+            gap: "1rem",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <Button
+            color="inherit"
+            variant="outlined"
+            onClick={handleClose}
+            sx={{
+              ...buttonStyle,
+              backgroundColor: "#E8DEF8",
+            }}
+          >
+            Cancel
+          </Button>
+
+          <Button
+            color="error"
+            onClick={(e) => {
+              onDelete(e);
+              handleClose();
+            }}
+            variant="contained"
+            sx={{
+              ...buttonStyle,
+              backgroundColor: "#B3261E",
+            }}
+          >
+            Delete
+          </Button>
+        </Box>
+      </Box>
+    </Modal>
+  );
+};
+
+export default DeleteModal;
